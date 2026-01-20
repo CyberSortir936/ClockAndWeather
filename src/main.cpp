4037d3c0aa7b44852ba8a120f0f51d91d7b9c3ab
CyberSortir936 <wixygamehd@gmail.com>
2026-01-20 13:37:09 +0200

    weather api test

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,54 @@
 #include <HTTPClient.h>
 #include "../lib/credential.h"
 #include "Adafruit_SSD1306.h"
+#include "Adafruit_GFX.h"
+
+#define BUTTON_PIN 4
 
 #define SDA_PIN 21
 #define SCL_PIN 22
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64  
+
+#define OLED_RESET     -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//DHT config
 #define DHTPIN 5
-
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* City = "Lviv";
-const char* CountryCode = "UA";
+// WeatherAPI config
+String City = "Lviv";
+String CountryCode = "UA";
+String jsonBuffer;
 
+String httpGETRequest(const char* serverName);
 
 void setup() {
   Serial.begin(115200);
   Serial.println(F("DHTxx test!"));
-
   dht.begin();
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  WiFi.begin(ssid, pass);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  byte buttonState = digitalRead(BUTTON_PIN);
   // Wait a few seconds between measurements.
   delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
@@ -43,10 +61,67 @@ void loop() {
     return;
   }
 
-
-  Serial.print(F("Humidity: "));
+  Serial.print(F("Inside Humidity: "));
   Serial.print(h);
-  Serial.print(F("%  Temperature: "));
+  Serial.print(F("%  Inside Temperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
+  Serial.println();
+
+
+  if (buttonState == LOW) {
+    // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      String serverPath = "http://api.openweathermap.org/data/2.5/weather?q=" + City + "," + CountryCode + "&APPID=" + weather_api_key;
+      
+      jsonBuffer = httpGETRequest(serverPath.c_str());
+      //Serial.println(jsonBuffer);
+      JSONVar myObject = JSON.parse(jsonBuffer);
+  
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+
+      Serial.print("Temperature: ");
+      Serial.println(myObject["main"]["temp"]);
+      Serial.print("Humidity: ");
+      Serial.println(myObject["main"]["humidity"]);
+      Serial.print("Sky: ");
+      Serial.println(myObject["weather"][0]["description"]);
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
 }
